59f40a95ffecc75eb96c14d743e8f86b075fb628
xieyongheng <x2914213073@qq.com>
2024-10-15 20:41:19 +0800

    Site updated: 2024-10-15 20:41:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/08/2024/10/JavaSSM03/","2024/10/06/2024/10/JavaSSM02/","2024/10/06/2024/10/JavaSSM01/","2024/10/10/2024/10/JavaSSM05/","2024/10/09/2024/10/JavaSSM04/","2024/10/11/2024/10/JavaSSM06/","2024/10/13/2024/10/JavaSpringProject01/","2024/10/12/2024/10/JavaTool/"];function toRandomPost(){
+var posts=["2024/10/09/2024/10/JavaSSM04/","2024/10/08/2024/10/JavaSSM03/","2024/10/10/2024/10/JavaSSM05/","2024/10/11/2024/10/JavaSSM06/","2024/10/06/2024/10/JavaSSM02/","2024/10/06/2024/10/JavaSSM01/","2024/10/13/2024/10/JavaSpringProject01/","2024/10/12/2024/10/JavaTool/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
